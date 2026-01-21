d5ee6e755170d61d69dda5bd15ac6ae210a432ae
eike-sakurai <155107207+eike-sakurai@users.noreply.github.com>
2026-01-20 22:23:12 -0300

    idk

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,6 @@
   users.users.eike-sakurai = {
     isNormalUser = true;
     extraGroups = [ "wheel" "input" "networkmanager"];
-    initialHashedPassword = "$6$cfgeVuIGlHfR9JxA$ZKr2AWIWFXw0RUfmkXyTfkCcFQg1SeglhDA6afZSoBMT4OlkCJPYiEL.WUjuCo/AL5WORCQLse88fYhmNHvL1";
   };
 
   environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
